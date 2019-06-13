4aac6e44fcb63fdbeba55e0583d5c6c3568b4d89
George Roberts <george.e.roberts@gmail.com>
2019-06-13 17:16:47 +0100

    Add code comments

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 "use strict";
 
+/* ------------------------
+FETCH FUNCTIONS
+-------------------------*/
+
 fetch('https://randomuser.me/api/1.2/?results=12')
   .then(res => res.json())
   .then(data => console.log(data));
+
+
+/*---------------------------------
+  HELPER FUNCTIONS
+----------------------------------*/ 
+
